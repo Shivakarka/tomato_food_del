e2945bcca3a12e1c6d74ec5e34b6d864829b661e
shivakarka <shivakarka@gmail.com>
2024-06-17 23:39:51 +0530

    Add Navbar component

---
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,49 @@
+import "./navbar.css";
+import { assets } from "../../assets/assets.ts";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [menu, setMenu] = useState("home");
+
+  return (
+    <div className="navbar">
+      <img src={assets.logo} alt="" className="log" />
+      <ul className="navbar-menu">
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          Home
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          Menu
+        </li>
+        <li
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          Mobile-app
+        </li>
+        <li
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          Contact-us
+        </li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="search icon" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="cart icon" />
+          <div className="dot"></div>
+        </div>
+        <button>Sign in</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
