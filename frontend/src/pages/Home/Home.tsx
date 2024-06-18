0ae95ea8ee03ceeba23c336461ae0302e2cca566
shivakarka <shivakarka@gmail.com>
2024-06-18 19:33:01 +0530

    Add ExploreMenu component

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,10 +1,15 @@
-import Header from "../../components/Header/Header";
+import { useState } from "react";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu.tsx";
+import Header from "../../components/Header/Header.tsx";
 import "./home.css";
 
 const Home = () => {
+  const [category, setCategory] = useState<string>("All");
+
   return (
     <div>
       <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
     </div>
   );
 };
