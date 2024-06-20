8753bd61e77dbae08ae9c05d17c9bc4655df7802
shivakarka <shivakarka@gmail.com>
2024-06-20 14:33:58 +0530

    Add functionality to add or remove items from cart

--- frontend/src/components/FoodItem/FoodItem.tsx
+++ frontend/src/components/FoodItem/FoodItem.tsx
@@ -1,40 +1,61 @@
+import { useContext, useState } from "react";
+import { assets } from "../../assets/assets";
 
-import { assets } from '../../assets/assets';
-
-import './foodItem.css';
+import "./foodItem.css";
+import { StoreContext, StoreContextType } from "../../context/StoreContext";
 
 type FoodItemProps = {
-    id: string,
-    name: string,
-    price: number,
-    description: string,
-    image: string
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+};
 
-}
+const FoodItem = ({ id, name, price, description, image }: FoodItemProps) => {
+  const { cartItems, addToCart, removeFromCart } = useContext(
+    StoreContext
+  ) as StoreContextType;
 
-
-const FoodItem = ({name,price,description,image}:FoodItemProps) => {
-
-
-    return (
-        <div className='food-item'>
-            <div className="food-item-img-container">
-                <img className='food-item-image' src={image} alt="" />
-
-            </div>
-            <div className="food-item-info">
-                <div className="food-item-name-rating">
-                    <p>{name}</p>
-                    <img src={assets.rating_starts} alt="" />
-                </div>
-                <p className="food-item-desc">
-                    {description}
-                </p>
-                <p className="food-item-price">${price}</p>
-            </div>
+  return (
+    <div className="food-item">
+      <div className="food-item-img-container">
+        <img className="food-item-image" src={image} alt="" />
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="" />
         </div>
+        <p className="food-item-desc">{description}</p>
+        <div className="food-item-price-counter">
+          <p className="food-item-price">${price}</p>
+          {!cartItems[id] ? (
+            <img
+              src={assets.add_icon_white}
+              alt="add item icon"
+              className="add"
+              onClick={() => addToCart(id)}
+            />
+          ) : (
+            <div className="food-item-counter">
+              <img
+                src={assets.remove_icon_red}
+                alt="item remove icon"
+                onClick={() => removeFromCart(id)}
+              />
+              <p>{cartItems[id]}</p>
+              <img
+                src={assets.add_icon_green}
+                alt="item add icon"
+                onClick={() => addToCart(id)}
+              />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default FoodItem
+export default FoodItem;
--- frontend/src/context/StoreContext.tsx
+++ frontend/src/context/StoreContext.tsx
@@ -1,24 +1,49 @@
-import React, {createContext} from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets.ts";
-import {FoodItemType} from "../../types";
+import { FoodItemType } from "../../types";
 
-type StoreContextType = {
-    food_list: FoodItemType[]
-}
+export type StoreContextType = {
+  food_list: FoodItemType[];
+  cartItems: Record<string, number>;
+  setCartItems: React.Dispatch<React.SetStateAction<Record<string, number>>>;
+  addToCart: (itemId: string) => void;
+  removeFromCart: (itemId: string) => void;
+};
 
 export const StoreContext = createContext<StoreContextType | null>(null);
 
-const StoreContextProvider = (props:{children: React.ReactNode}) => {
+const StoreContextProvider = (props: { children: React.ReactNode }) => {
+  const [cartItems, setCartItems] = useState<Record<string, number>>({});
 
-    const contextValue = {
-        food_list
+  const addToCart = (itemId: string) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
+  };
+
+  const removeFromCart = (itemId: string) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  useEffect(() => {
+    console.log(cartItems);
+  }, [cartItems]);
+
+  const contextValue = {
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+  };
 
   return (
     <StoreContext.Provider value={contextValue}>
       {props.children}
     </StoreContext.Provider>
   );
-}
+};
 
 export default StoreContextProvider;
