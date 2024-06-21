471cc9f452e3912228a834180cf83ca0ed6d91d7
shivakarka <shivakarka@gmail.com>
2024-06-22 00:16:55 +0530

    create basic backend setup

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+// App config
+const app = express();
+const port = 4000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on localhost:${port}`);
+});
