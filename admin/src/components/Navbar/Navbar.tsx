e0461a26a9ef3a4e1df81ddc0fd212f2b081386a
shivakarka <shivakarka@gmail.com>
2024-06-23 16:05:03 +0530

    created navbar

--- admin/src/components/Navbar/Navbar.tsx
+++ admin/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,13 @@
+import { assets } from "../../assets/assets.ts";
 import "./navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <img src={assets.logo} alt="logo" className="logo" />
+      <img src={assets.profile_image} alt="profile image" className="profile" />
+    </div>
+  );
 };
 
 export default Navbar;
