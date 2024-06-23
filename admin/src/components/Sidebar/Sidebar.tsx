d1150c571dd8444247da57856867e1ba9e0f9b77
shivakarka <shivakarka@gmail.com>
2024-06-23 17:05:50 +0530

    create sidebar

--- admin/src/components/Sidebar/Sidebar.tsx
+++ admin/src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,25 @@
+import { assets } from "../../assets/assets";
 import "./sidebar.css";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar-options">
+        <div className="sidebar-option">
+          <img src={assets.add_icon} alt="add icon" />
+          <p>Add Items</p>
+        </div>
+        <div className="sidebar-option ">
+          <img src={assets.list_icon} alt="list icon" className="list-icon" />
+          <p>List Items</p>
+        </div>
+        <div className="sidebar-option">
+          <img src={assets.order_icon} alt="order icon" />
+          <p>Orders</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
