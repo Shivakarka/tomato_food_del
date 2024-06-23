464986fd7a3493ecc02349dffaf23af684d9675c
shivakarka <shivakarka@gmail.com>
2024-06-23 15:18:42 +0530

    created required component files

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,5 +1,16 @@
+import Navbar from "./components/Navbar/Navbar.tsx";
+import Sidebar from "./components/Sidebar/Sidebar.tsx";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+      </div>
+    </>
+  );
 }
 
 export default App;
