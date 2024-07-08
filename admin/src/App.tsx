b1324df0ba3365a597c1930b0a005386866fe1f5
shivakarka <shivakarka@gmail.com>
2024-07-08 18:37:17 +0530

    Implemented List component to view and delete products

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
         <Sidebar />
         <Routes>
           <Route path="/add" element={<Add url={url} />} />
-          <Route path="/list" element={<List />} />
+          <Route path="/list" element={<List url={url} />} />
           <Route path="/orders" element={<Orders />} />
         </Routes>
       </div>
--- admin/src/pages/List/List.tsx
+++ admin/src/pages/List/List.tsx
@@ -1,7 +1,62 @@
+import { useEffect, useState } from "react";
 import "./list.css";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { FoodItemType } from "../../types.ts";
 
-const List = () => {
-  return <div>List</div>;
+const List = ({ url }: { url: string }) => {
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    const response = await axios.get(`${url}/api/food/list`);
+    if (response.data.success) {
+      setList(response.data.data);
+    } else {
+      toast.error("Error");
+    }
+  };
+
+  const removeFood = async (foodId: string) => {
+    const response = await axios.delete(`${url}/api/food/delete/${foodId}`);
+    await fetchList();
+    if (response.data.success) {
+      toast.success(response.data.message);
+    } else {
+      toast.error("Error");
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  return (
+    <div className="list add flex-col">
+      <p>All Foods List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list?.map((item: FoodItemType, index) => {
+          return (
+            <div key={index} className="list-table-format">
+              <img src={`${url}/images/` + item?.image} alt="food image" />
+              <p>{item?.name}</p>
+              <p>{item?.category}</p>
+              <p>${item?.price}</p>
+              <p onClick={() => removeFood(item?._id)} className="cursor">
+                X
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default List;
