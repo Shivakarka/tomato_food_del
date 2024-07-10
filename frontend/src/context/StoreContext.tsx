da55890634b9eb804b824891e71ec92387cd73a8
shivakarka <shivakarka@gmail.com>
2024-07-10 19:28:14 +0530

    Refactored code to fetch and update cart data in backend

--- frontend/src/context/StoreContext.tsx
+++ frontend/src/context/StoreContext.tsx
@@ -10,16 +10,38 @@ const StoreContextProvider = (props: { children: React.ReactNode }) => {
   const [token, setToken] = useState<string | null>(null);
   const [foodList, setFoodList] = useState<FoodItemType[]>([]);
 
-  const addToCart = (itemId: string) => {
+  const addToCart = async (itemId: string) => {
     if (!cartItems[itemId]) {
       setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
     } else {
       setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
+    if (token) {
+      try {
+        await axios.post(
+          `${url}/api/cart/add`,
+          { itemId },
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    }
   };
 
-  const decreaseItemFromCart = (itemId: string) => {
+  const decreaseItemFromCart = async (itemId: string) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+    if (token) {
+      try {
+        await axios.post(
+          `${url}/api/cart/remove`,
+          { itemId },
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    }
   };
 
   const getTotalCartAmount = () => {
@@ -38,8 +60,23 @@ const StoreContextProvider = (props: { children: React.ReactNode }) => {
   };
 
   const fetchFoodList = async () => {
-    const response = await axios.get(`${url}/api/food/list`);
-    setFoodList(response.data.data);
+    try {
+      const response = await axios.get(`${url}/api/food/list`);
+      setFoodList(response.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const loadCart = async (token: string) => {
+    try {
+      const response = await axios.get(`${url}/api/cart`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      setCartItems(response.data.cartData);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
@@ -47,6 +84,7 @@ const StoreContextProvider = (props: { children: React.ReactNode }) => {
     const token = localStorage.getItem("token");
     if (token) {
       setToken(token);
+      loadCart(localStorage.getItem("token") as string);
     }
   }, []);
 
