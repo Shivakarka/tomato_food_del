eaae68ee118cd058e6481534d876945e8f03c76d
shivakarka <shivakarka@gmail.com>
2024-07-11 20:24:58 +0530

    Created payment verify page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
-import Home from "./pages/Home/Home";
-import Cart from "./pages/Cart/Cart";
-import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
-import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar.tsx";
+import Home from "./pages/Home/Home.tsx";
+import Cart from "./pages/Cart/Cart.tsx";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder.tsx";
+import Footer from "./components/Footer/Footer.tsx";
 import { useEffect, useState } from "react";
-import LoginPopup from "./components/LoginPopup/LoginPopup";
+import LoginPopup from "./components/LoginPopup/LoginPopup.tsx";
+import Verify from "./pages/Verify/Verify.tsx";
 
 function App() {
   const [showLogin, setShowLogin] = useState(false);
@@ -23,6 +24,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<PlaceOrder />} />
+          <Route path="/verify" element={<Verify />} />
         </Routes>
       </div>
       <Footer />
---
+++ frontend/src/pages/Verify/Verify.tsx
@@ -0,0 +1,54 @@
+import { useContext, useEffect, useState } from "react";
+import "./verify.css";
+import { StoreContext } from "../../context/StoreContext.tsx";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import axios from "axios";
+
+const Verify = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const success = searchParams.get("success");
+  const orderId = searchParams.get("orderId");
+  const { url } = useContext(StoreContext)!;
+  const navigate = useNavigate();
+  const [error, setError] = useState(false);
+
+  const verifyPayment = async () => {
+    try {
+      const response = await axios.post(url + "/api/order/verify", {
+        success,
+        orderId,
+      });
+      if (response.data.success) {
+        navigate("/myorders");
+      } else {
+        navigate("/");
+        alert(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    }
+  };
+
+  useEffect(() => {
+    verifyPayment();
+  }, []);
+
+  if (error) {
+    return (
+      <div className="error">
+        <h1>Something went wrong</h1>
+        <p>Please try again later</p>
+        <button onClick={() => navigate("/")}>Go Home</button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="verify">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default Verify;
