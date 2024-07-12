ddcfa9c4b7303d9d87cea4a8936094c9a8988a49
shivakarka <shivakarka@gmail.com>
2024-07-12 13:05:47 +0530

    changed some config

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("uploads"));
 
 // DB config
 connectDB();
