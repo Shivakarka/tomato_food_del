ef0259a9d8ffc13aa7cd4c55d11f2d69c4cef32c
shivakarka <shivakarka@gmail.com>
2024-07-13 19:41:21 +0530

    Added CORS origin

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,12 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://tomato-food-del.vercel.app", // your frontend URL or use localhost
+    methods: "GET,POST,PUT,DELETE", // allowed methods
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
